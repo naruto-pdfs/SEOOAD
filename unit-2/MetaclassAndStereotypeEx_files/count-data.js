d5881039638124f4aaea56d38059ff92b0a0a542
naruto-pdfs <snistpms@gmail.com>
2020-02-22 11:54:36 +0530

    Initial commit

---
+++ unit-2/MetaclassAndStereotypeEx_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"http:\/\/www.uml-diagrams.org\/profile-diagrams.html","comments":10}]});
+}
